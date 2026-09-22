64600e6cd2af0d59ccbb1c900e3c5a97d559893c
Milo34 <BlairK34@Live.com>
2026-09-21 21:44:32 -0600

    Stage LotKeys 0.9.4.99 complete source

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lotkeys-app-v09498-stable-81-route-sync-repair';
+const CACHE='lotkeys-app-v09499-indexeddb-navigation-cache';
 const LOTKEYS_CACHE_PREFIXES=['lotkeys-drive-test-','lotkeys-app-'];
 const CORE=[
   './',
